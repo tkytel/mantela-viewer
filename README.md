7a7229db8e498460c9fe8b00a30af67917c75a1b
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2024-11-28 11:15:01 +0900

    First draft
    Co-authored-by: m-tsuru <147146974+m-tsuru@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mantela &ndash; Telephone Network Mandala
---
+++ mantela.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const nodes = [];
+const edges = [];
+
+async function
+main(first)
+{
+    if (!first)
+        return;
+
+    const queue = [ first ];
+    const visited = [];
+    while (queue.length > 0) {
+        try {
+            const cur = queue.shift();
+            const k = await (await fetch(cur, { mode: 'cors' })).json();
+            if (visited.some(e => e === cur))
+                continue;
+            visited.push(cur);
+
+            const me = {
+                id: k.aboutMe.identifier,
+                label: k.aboutMe.name,
+            };
+            if (!nodes.some(q => q.id === me.id)) {
+                nodes.push(me);
+            }
+
+            k.extensions.forEach(e => {
+                const node = {
+                    id: me.id + Math.random(),
+                    label: e.name,
+                    color: 'orange',
+                };
+                nodes.push(node);
+                const edge = {
+                    from: me.id,
+                    to: node.id,
+                    label: e.extension,
+                };
+                edges.push(edge);
+            });
+            k.providers.forEach(e => {
+                if (!nodes.some(q => q.id === e.identifier)) {
+                    const node = {
+                        id: e.identifier,
+                        label: e.name,
+                    };
+                    nodes.push(node);
+                }
+                const edge = {
+                    from: me.id,
+                    to: e.identifier,
+                    label: e.prefix,
+                };
+                edges.push(edge);
+                queue.push(e.mantela);
+            });
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    const container = document.getElementById('mandala');
+    const data = {
+        nodes: nodes,
+        edges: edges,
+    };
+    const options = {
+        edges: {
+            arrows: 'to',
+        },
+    };
+
+    new vis.Network(container, data, options);
+}
+
+const q = (new URLSearchParams(document.location.search)).get('first');
+if (q) {
+    first.value = q;
+    main(first.value);
+}
