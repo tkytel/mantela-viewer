f06d233b52ed46d8da7304b6ca80098f9443cbf6
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2025-05-29 19:40:28 +0900

    [WIP] 高画質モード

--- mantela.js
+++ mantela.js
@@ -252,11 +252,14 @@ graph2vis(container, graph)
 			arrows: 'to',
 		},
 		layout: {
+			randomSeed: 0xDEADBEEF,
 			improvedLayout: false,
 		},
 		physics: {
 			solver: 'forceAtlas2Based',
 		},
+		width: '400%',
+		height: '400%',
 	};
 
 	return new vis.Network(container, data, options);
@@ -459,6 +462,9 @@ formMantela.addEventListener('submit', async e => {
 	});
 	network.on('stabilizationIterationsDone', _ => {
 		divMantelaCover.style.display = 'none';
+	});
+	network.on('stabilized', async e => {
+		console.log('stabilized!');
 	});
 	secMandala.scrollIntoView({
 		behavior: 'smooth',
